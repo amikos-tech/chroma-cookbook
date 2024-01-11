904e3edf56ba5cb41fc7a198c6c37475f9a07efb
tazarov <tazarov@users.noreply.github.com>
2024-01-11 11:49:20 +0000

    deploy: d3e08a7f68824c285a91afb94198a237b2308110

--- javascripts/gtag.js
+++ javascripts/gtag.js
@@ -1,8 +1,8 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-FZFKK5FLEY"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FZFKK5FLEY"/>
 <script>
     window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
+    function gtag(){dataLayer.push(arguments)}
     gtag('js', new Date());
 
     gtag('config', 'G-FZFKK5FLEY');
